606cf1b32207384cf982a22ee48aa8dd43968bc0
lhl <edisonleolhl@foxmail.com>
2021-01-14 16:16:55 +0800

    finish dynamic joining and leaving

--- dynamic_iteration_pricing.m
+++ dynamic_iteration_pricing.m
@@ -1,13 +1,14 @@
-% 先两个用户迭代，然后第100轮第三个用户加入
+% 先两个用户迭代，然后第200轮第三个用户加入，第400轮第三个用户离开
 function main()
     bw_step = 0.01;
     price_step = 0.01;
     global repu; repu = 10;
-    global MAX_ITER; MAX_ITER = 500;
+    global MAX_ITER; MAX_ITER = 600;
+    global MAX_EPOCH; MAX_EPOCH = 200;
     global CAPACITY; CAPACITY = 10;
     global NUMBERS; NUMBERS = 3;
-    global will; will = [2, 2, 2];
-    global qos; qos = [1, 5, 1];
+    global will; will = [1, 2, 3];
+    global qos; qos = [1, 1, 1];
     global BW; BW = [0, 0, 0];
     global PRICE; PRICE = [0.1, 0.1, 0.0];
     REVENUE_History = zeros(1, MAX_ITER);
@@ -20,7 +21,7 @@ function main()
         revenue = 0;
         % 1. ISP更新定价策略
         for i=1:NUMBERS
-            % 第三个用户前一百轮迭代不参与
+            % 第三个用户前200轮迭代不参与，第200轮预测价格，后400轮迭代不参与
             if i == 3
                 if time < 200
                     continue
@@ -28,23 +29,30 @@ function main()
                     PRICE(i) = PRICE(min_distance_index());
                     fprintf('预测价格为%f\n', PRICE(i));
                     continue;
+                elseif time >= 400
+                    PRICE(i) = 0;
+                    continue;
                 end
             end
-            PRICE(i) = PRICE(i) + max(0, price_step*cal_change_rate_p(i));
+            fprintf('总带宽为%f\n', sum(BW));
+            PRICE(i) = max(0, PRICE(i) + price_step*cal_change_rate_p(i));
         end
         % 2. 单个用户通过迭代调节自己的带宽策略，直到自己的效用函数达到最大值，因为效用函数是凹函数，所以肯定会收敛
         for i=1:NUMBERS
-            % 第三个用户前一百轮迭代不参与
-            if i == 3 && time < 200
-                continue
+            % 第三个用户前200轮迭代不参与，后400轮迭代不参与
+            if i == 3 
+                if time < 200
+                    continue
+                elseif time >= 400
+                    BW(i) = 0;
+                    continue;
+                end
             end
             utility = cal_utility(i, BW(i), PRICE(i));
             next_bw = max(0, BW(i) + bw_step*cal_change_rate_b(i));
             next_utility = cal_utility(i, next_bw, PRICE(i));
-            k = 1;
-            %for k=1:MAX_ITER
-            fprintf('-------%3d epoch----------\n',k);
-            while abs(utility - next_utility) > 10^(-3)
+            for k=1:MAX_EPOCH
+                %fprintf('-------%3d epoch----------\n',k);
                 BW(i) = next_bw;
                 if time == 1
                     UTILITY_Epoch_History(i, k) = utility;
@@ -61,7 +69,6 @@ function main()
             BW_History(i, time) = BW(i);
         end
         fprintf('network revenue = %f\n' , revenue);
-        % 3. ISP收益函数不是凹函数，得看是否收敛
         REVENUE_History(time) = revenue;
         PRICE_History(1, time) = PRICE(1);
         PRICE_History(2, time) = PRICE(2);
@@ -92,7 +99,6 @@ function plot_price_dynamic(x, m, PRICE_History)
         x(m), PRICE_History(2, m), 'kx',  x(m), PRICE_History(3, m), 'bp');
     legend({'ISP对用户1定价策略', 'ISP对用户2定价策略', 'ISP对用户3定价策略'}, 'Location', 'northwest', 'FontSize', 10);
     xlabel('迭代次数','FontSize', 15);
-    %ylabel('效益','FontSize', 15);
     set(0,'DefaultFigureWindowStyle','docked')
 end
 
@@ -104,7 +110,6 @@ function plot_bw_dynamic(x, m, BW_History)
     plot_pb_markers = plot(x(m), BW_History(1, m), 'rd', x(m), BW_History(2, m), 'c+', x(m), BW_History(3, m), 'ms');
     legend({'用户1带宽策略', '用户2带宽策略', '用户3带宽策略'}, 'Location', 'northeast', 'FontSize', 10);
     xlabel('迭代次数','FontSize', 15);
-    %ylabel('效益','FontSize', 15);
     set(0,'DefaultFigureWindowStyle','docked')
 end
 
@@ -142,14 +147,19 @@ function rb=cal_change_rate_b(i)
     global repu;
     global CAPACITY;
     rb = will(i)*repu*qos(i)/(1+repu*qos(i)*BW(i)) - PRICE(i);
+    % 吞吐量大于网络容量，所以减少用户的带宽
+    if sum(BW) > CAPACITY
+        rb = -abs(rb);
+    end
 end
 
 function rp=cal_change_rate_p(i)
     global BW;
     global CAPACITY;
     rp = BW(i);
+    % 吞吐量太小，所以减少价格，诱导用户带宽需求增加
     if sum(BW) < CAPACITY / 5
-        rp = -rp;
+        rp = -abs(rp);
     end
 end
 
