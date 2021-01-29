9ac94aeb69b41799bf12192fa9fdc55ce4ee0f39
Edison Leo <edisonleolhl@foxmail.com>
2021-01-29 14:41:53 +0800

    finish user numbers effect on price&bw and revenue&utility

--- numbers_effect.m
+++ numbers_effect.m
@@ -11,14 +11,18 @@ function main()
     global BW; BW = zeros(1, 30); % 三十个用户，初始带宽为0
     global PRICE; PRICE = 0.1*ones(1, 30); % 网络j的初始价格
     global NUMBERS; NUMBERS = 3; % 初始测试三个用户
-    revenue_trends = zeros(1, 10);
-    x = [3:3:30];
-    for num=x
+    n_list = [3:30];
+    n_num = length(n_list);
+    revenue_trends = zeros(1, n_num);
+    avg_price_trends = zeros(1, n_num);
+    avg_bw_trends = zeros(1, n_num);
+    avg_utility_trends = zeros(1, n_num);
+    for num=n_list
         fprintf('-------------------%3d users----------------\n',num);
-        % 回归初始条件
+        NUMBERS = num;
+        % 回归初始条件，一定要先修改BW、PRICE数组
         BW = zeros(1, num);
         PRICE = 0.1*ones(1, num);
-        NUMBERS = num;
         for time=1:MAX_ITER
             % fprintf('-------------------%3d round----------------\n',time);
             revenue = 0;
@@ -40,28 +44,56 @@ function main()
                     k = k + 1;
                 end
                 revenue = revenue + cal_revenue(BW(i), PRICE(i));
-                if time == MAX_ITER
-                    fprintf('bw = %f, price = %f\n' ,BW(i), PRICE(i));
-                end
-            end
-            if time == MAX_ITER
-                fprintf('network revenue = %f\n' , revenue);
             end
         end
-        revenue_trends(num/3) = revenue;
+        avg_price = mean(PRICE(1:NUMBERS));
+        avg_bw = mean(BW(1:NUMBERS));
+        avg_utility = cal_utility(1, avg_bw, avg_price); % w与q都相同，所以i=1即可
+        fprintf('avg_bw = %f, avg_price = %f\n' , avg_bw, avg_price);
+        fprintf('avg_utility = %f\n' , avg_utility);
+        fprintf('network revenue = %f\n' , revenue);
+        avg_price_trends(num-2) = avg_price;
+        avg_bw_trends(num-2) = avg_bw;
+        avg_utility_trends(num-2) = avg_utility;
+        revenue_trends(num-2) = revenue;
     end
     fprintf('----------ENDING-----------\n');
     figure;
-    plot_numbers_effect_on_revenue(x, revenue_trends);
-    savefig('plot_numbers_effect_on_revenue');
+    plot_numbers_effect_on_pb(n_list, avg_price_trends, avg_bw_trends);
+    savefig('plot_numbers_effect_on_pb');
+    figure;
+    plot_numbers_effect_on_ru(n_list, revenue_trends, avg_utility_trends);
+    savefig('plot_numbers_effect_on_ru');
 end
 
-function plot_numbers_effect_on_revenue(x, revenue_trends)
-    plot_revenue = plot(x, revenue_trends, 'r-*');
-    legend({'ISP收益'},'FontSize', 15, 'Location', 'northwest');
-    xlim([0 35]);
+function plot_numbers_effect_on_pb(x, avg_price_trends, avg_bw_trends)
+    yyaxis left
+    plot(x, avg_price_trends, 'b-p');
+    yyaxis right
+    plot(x, avg_bw_trends, 'r-*');
+    legend({'ISP对用户的平均定价策略', '用户平均带宽策略'}, ...,
+        'FontSize', 15, 'Location', 'northwest');
+    xlim([3 30]);
+    yyaxis left
+    ylabel('价格','FontSize', 15);
+    yyaxis right
+    ylabel('带宽','FontSize', 15);
     xlabel('用户数','FontSize', 15);
-    ylabel('效益','FontSize', 15);
+    set(0,'DefaultFigureWindowStyle','docked');
+end
+
+function plot_numbers_effect_on_ru(x, revenue_trends, avg_utility_trends)
+    yyaxis left
+    plot(x, avg_utility_trends, 'b-p');
+    yyaxis right
+    plot(x, revenue_trends, 'r-*');
+    legend({'用户平均效用', 'ISP收益'},'FontSize', 15, 'Location', 'north');
+    xlabel('用户数','FontSize', 15);
+    yyaxis left
+    ylabel('效用','FontSize', 15);
+    yyaxis right
+    ylabel('收益','FontSize', 15);
+    xlim([3 30]);
     set(0,'DefaultFigureWindowStyle','docked');
 end
 
